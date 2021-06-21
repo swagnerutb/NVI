86b1652f499c1242c3bac562fb2b3ed602d78e1a
swagnerutb <77800661+swagnerutb@users.noreply.github.com>
2021-06-21 10:49:21 +0200

    improved GUI

--- proj1/run_vgosDB.py
+++ proj1/run_vgosDB.py
@@ -1,53 +1,93 @@
+import sys
 import os
-import inquirer
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import QObject, QRectF, Qt
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QWidget, QCheckBox, QVBoxLayout, QLineEdit, QLabel, QPushButton
 
-def ask_user_wrp(path):
-    """
-    User selection of path to .wrp file
-    """
-    dir_cont = ['### return ###']
-    dir_cont_wrp = []
-    dir_cont_dir = []
-    for content in os.listdir(path):
-        if(content.endswith('.wrp')):
-            dir_cont_wrp.append(content)
-        elif(os.path.isdir(os.path.join(path,content))):
-            dir_cont_dir.append(content)
+# Check OS
+import platform
+if(platform.system() == 'Darwin'): #if running on mac
+    os.environ['QT_MAC_WANTS_LAYER'] = '1'
 
-    dir_cont.extend(dir_cont_wrp) #Always get .wrp files first
-    dir_cont.extend(dir_cont_dir)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        self.setWindowTitle("Initialize vgosDBpy")
+
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.gridLayout = QtWidgets.QGridLayout(self.centralWidget)
+
+
+        self.nameLabel = QLabel(self.centralWidget)
+        self.line = QLineEdit(self.centralWidget)
+        self.file_select_button = QPushButton(self.centralWidget)
+        self.graph_check = QtWidgets.QCheckBox(self.centralWidget)
+        self.run_prog = QPushButton(self.centralWidget)
+
+        self.line.setGeometry(QtCore.QRect(140, 25, 300, 20))
+        self.nameLabel.setGeometry(QtCore.QRect(20, 25, 100, 20))
+
+        self.file_select_button.setGeometry(QtCore.QRect(10, 10, 100, 100))
+        self.graph_check.setGeometry(QtCore.QRect(10, 10, 200, 200))
+        self.run_prog.setGeometry(QtCore.QRect(10, 10, 200, 200))
+        
+        self.nameLabel.setText("Selected .wrp file:")
+        self.file_select_button.setText("Load .wrp file")
+        self.graph_check.setText("Run the graphical interface")
+        self.run_prog.setText("Run")
+
+        self.gridLayout.addWidget(self.nameLabel,0,0)
+        self.gridLayout.addWidget(self.line,0,2)
+        self.gridLayout.addWidget(self.file_select_button,1,0)
+        self.gridLayout.addWidget(self.graph_check,1,2)
+        self.gridLayout.addWidget(self.run_prog,3,0,1,4)
+
+        self.graph_intface = False
+        
+        MainWindow.setCentralWidget(self.centralWidget)
+
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        Ui_MainWindow.__init__(self)
+        QMainWindow.__init__(self)
+
+        # Initialize UI
+        self.setupUi(self)
+        self.file_select_button.clicked.connect(self._file_path)
+        self.graph_check.setCheckState(QtCore.Qt.Unchecked)
+        self.graph_check.stateChanged.connect(self._check_graph)
+        self.run_prog.clicked.connect(self._run_vgosDB)
+
+    def tr(self, text):
+        return QObject.tr(self, text)
+
+    def _file_path(self):
+        path = os.environ.get('VGOSDB_DIR')
+        path_to_wrp, _ = QFileDialog.getOpenFileName(self, self.tr("Load .wrp file"), self.tr(path), self.tr("Wrap files (*.wrp)"))
+        
+        if(path_to_wrp != ""):
+            self.line.setText(path_to_wrp)
     
-    if(len(dir_cont) == 1):
-        print('#####\nYou have entered a directory with no subdirectories or .wrp files. Please select another directory.\n#####')
-        return ask_user_wrp(os.path.dirname(path))
-    
-    question = [inquirer.List('select',message='Please select a .wrp file or directory', choices=dir_cont)]
-    answer = inquirer.prompt(question)['select']
-    
-    if(answer == '### return ###'):
-        return ask_user_wrp(os.path.dirname(path))
-    elif(answer.endswith('.wrp')):
-        return os.path.join(path,answer)
-    elif(os.path.isdir(os.path.join(path,answer))):
-        return ask_user_wrp(os.path.join(path,answer))
+    def _check_graph(self):
+        self.graph_intface = (False == self.graph_intface)
 
-def ask_user_g():
-    """
-    If user wishes to run the graphical interface.
-    """
-    questions = [inquirer.List('y_n',message='Would you like to open the graphical interface?', choices=["yes", "no"])]
-    return inquirer.prompt(questions)['y_n']
+    def _run_vgosDB(self):
+        cmd = "python -m vgosDBpy "+self.line.text()
+        cmd = "python3 -m vgosDBpy "+self.line.text()
+        if(self.graph_intface == True):
+            cmd = cmd+" -g"
+        
+        app.quit()
+        os.system(cmd)
 
-path = os.environ.get('VGOSDB_DIR')
 
-wrp_dir = ask_user_wrp(path)
+if __name__ == "__main__":
+    path = os.environ.get('VGOSDB_DIR')
 
-graph_flag = ask_user_g()
-
-os_cmd = 'python -m vgosDBpy ' + wrp_dir
-#os_cmd = 'python3 -m vgosDBpy ' + wrp_dir
-
-if(graph_flag == "yes"):
-    os_cmd = os_cmd+" -g"
-
-os.system(os_cmd)
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
